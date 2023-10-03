ebda9724f36c1238770bdc013146d3ef4a92f673
Pawel <pawel.bornikowski@gmail.com>
2023-10-04 01:42:39 +0200

    create banner with dynamic data

--- components/banner/Banner.jsx
+++ components/banner/Banner.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
-const Banner = ({banner}) => {
- 
+import React from "react";
+import { urlFor } from "@/sanity/lib/client";
+import Image from "next/image";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+const Banner = ({ banner }) => {
   return (
-    <div className="w-full bg-grey min-h-[500px] rounded-3xl"></div>
-  )
-}
+    <div className="w-full bg-grey min-h-[500px] md:min-h-[600px] rounded-3xl flex flex-col lg:flex-row">
+      <div className="lg:w-1/2 order-2 lg:order-1 p-4 px-8 md:p-16 flex flex-col justify-between lg:justify-around items-center lg:items-start gap-8">
+        <div className="">
+          <h5 className="w-full">{banner.smallText}</h5>
+          <h1>{banner.bigText}</h1>
+          <div className="w-full md:w-4/5">
+            <h5>Description</h5>
+            <p className="text-sm text-left font-thin">{banner.desc}</p>
+          </div>
+        </div>
 
-export default Banner
+        <button
+          type="button"
+          className="hidden md:block w-64 rounded-2xl bg-blue text-2xl p-4"
+        >
+          {banner.buttonText}
+        </button>
+        <button
+          type="button"
+          className="md:hidden rounded-full bg-blue w-16 h-16 flex justify-center items-center"
+        >
+          <AiOutlineShoppingCart className="text-3xl" />
+        </button>
+      </div>
+      <div className="lg:w-1/2 order-1 lg:order-2 flex justify-center items-center">
+        <img src={urlFor(banner.image[0]).width(700)} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- components/heading/HeadingText.jsx
+++ components/heading/HeadingText.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeadingText = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center h-52 gap-2">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Best Seller Products</h1>
+        <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">Best Seller Products</h1>
         <p className="text-center text-xs md:text-sm lg:text-base w-3/5">Elevate Your Audio Experience with Our Top Headphones</p>
     </div>
   )
