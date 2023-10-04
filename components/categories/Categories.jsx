cfc7fbb5d2b7ea5917acc61aabb8db0850c9a817
Pawel <pawel.bornikowski@gmail.com>
2023-10-05 00:01:36 +0200

    add onclick event to categories navigation

--- components/categories/Categories.jsx
+++ components/categories/Categories.jsx
@@ -1,19 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 const Categories = () => {
-    const categories = ['Wireless', 'Gaming', 'Earphones', "Speakers", "Watches"]
-    useEffect(() => {
-        const categories = document.querySelectorAll('.categories');
+  const categoriesArray = [
+    "Wireless",
+    "Gaming",
+    "Earphones",
+    "Speakers",
+    "Watches",
+  ];
+  const liClass = "categories font-bold md:self-center cursor-pointer" ;
 
-        categories[0].classList.add('active')
-    },[])
+  const handleClick = (id) => {
+    const categories = document.querySelectorAll(".categories");
+    categories.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    categories[id].classList.add("active");
+  };
+
+  useEffect(() => {
+    const categories = document.querySelectorAll(".categories");
+    categories[0].classList.add("active");
+  }, []);
   return (
     <div className=" my-16 md:text-lg">
-        <ul className=" h-24 flex justify-around p-4">
-            {categories.map((item, index) => <li className={index % 2 === 0 ? 'categories font-bold self-start md:self-center' : 'categories font-bold self-end md:self-center'} key={index}>{item}</li>)}
-        </ul>
+      <ul className=" h-24 flex justify-around p-4">
+        {categoriesArray.map((item, index) => (
+          <li
+            className={
+              index % 2 === 0 ? `${liClass} self-start` : `${liClass} self-end`
+            }
+            key={index}
+            onClick={() => handleClick(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
