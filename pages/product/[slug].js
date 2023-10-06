1a20688db000e9774c7e95c841be4c0b67cefb56
Pawel <pawel.bornikowski@gmail.com>
2023-10-06 11:01:30 +0200

    fix positioning and display in product cart, add toggle functionto small images of headphones

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { client, urlFor } from "@/sanity/lib/client";
 import { AiFillStar, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { useStateContext } from "@/context/StateContext";
@@ -6,27 +6,46 @@ import Quantity from "@/components/reusable/Quantity";
 const ProductDetails = ({ product, products }) => {
   const { incQty, decQty, qty, addItemToCart } = useStateContext();
   const { image, name, details, price, stars, reviews } = product;
+  const [bigImage, setBigImage] = useState(image[0]);
+
+  const toggleProduct = (image, index) => {
+    setBigImage(image);
+
+    const smallImg = document.querySelectorAll(".small_img");
+    smallImg.forEach((img) => {
+      img.classList.remove("small_img_active");
+    });
+    smallImg[index].classList.add("small_img_active");
+  };
+  useEffect(() => {
+    document.querySelectorAll(".small_img")[0].classList.add("small_img_active");
+   
+
+
+  }, []);
+
   return (
-    <div className=" w-full min-h-[500px] flex flex-col lg:flex-row gap-8">
-      <div className="flex flex-row lg:w-1/2 gap-2">
-        <div className="order-2 flex ">
-          <img
-            src={urlFor(image && image[0])}
-            alt=""
-            className="rounded-[20px] bg-grey"
-          />
-        </div>
-        <div className="w-28 lg:w-32 flex flex-col justify-between order-1">
-          {image?.map((img) => (
+    <div className=" w-full flex flex-col lg:flex-row gap-8 justify-center items-center lg:items-start">
+      <div className="flex lg:flex-col lg:w-6/12 gap-2">
+        <div className="w-28 lg:w-32 flex flex-col lg:flex-row lg:order-2 gap-4">
+          {image?.map((img, index) => (
             <img
               src={urlFor(img && img)}
               alt=""
-              className="bg-grey p-2 rounded-[20px]"
+              className="small_img bg-grey rounded-[20px]"
+              onMouseEnter={() => toggleProduct(img, index)}
             />
           ))}
         </div>
+        <div className=" flex lg:order-1">
+          <img
+            src={urlFor(bigImage)}
+            alt=""
+            className="rounded-[20px] bg-grey w-[600px]"
+          />
+        </div>
       </div>
-      <div className="w-full lg:w-1/2 flex flex-col items-start justify-evenly">
+      <div className="w-full lg:w-6/12 flex flex-col items-start justify-evenly gap-6 lg:gap-8">
         <h3 className="text-4xl font-medium lg:text-5xl">{name}</h3>
         <div className="flex items-center gap-4">
           <AiFillStar className="text-4xl md:text-5xl text-blue" />
@@ -41,8 +60,11 @@ const ProductDetails = ({ product, products }) => {
           <h5 className="font-bold">Quantity:</h5>
           <Quantity />
         </div>
-        <h4 className="font-bold drop-shadow-glow">${price}</h4>
-        <button className="p-4 w-40 bg-blue rounded-[30px]" onClick={() => addItemToCart(product, qty)}>
+        <h3 className="font-bold drop-shadow-glow">${price}</h3>
+        <button
+          className="p-4 w-40 bg-blue rounded-[30px]"
+          onClick={() => addItemToCart(product, qty)}
+        >
           Add to Cart
         </button>
       </div>
