4a7bf7a7f4696151152d5f3c2a46e88c1689261d
Pawel <pawel.bornikowski@gmail.com>
2023-10-15 01:01:02 +0200

    add hover to add to cart button inside product component

--- components/products/Product.jsx
+++ components/products/Product.jsx
@@ -10,13 +10,13 @@ const Product = ({
 }) => {
   // max-w-[320px] h-[200px] lg:w-[350px] md:h-[450px] rounded-[30px] flex flex-col justify-between relative  bg-grey z-0 hover:scale-110 duration-200
   return (
-    <div className="bg-grey relative flex items-center md:flex-col max-w-[400px] w-[320px] md:w-[350px] rounded-[20px]">
+    <div className="bg-grey relative flex items-center md:flex-col max-w-[400px] w-[320px] md:w-[350px] rounded-[20px] ">
       {hot ? (
-        <BsFire className="absolute right-0 top-0 text-3xl text-red-600" />
+        <BsFire className="absolute right-0 top-0 text-3xl text-red-600 md:right-4 md:top-4" />
       ) : null}
       <Link
         href={`/product/${category.toLowerCase()}/${slug.current}`}
-        className="flex-center"
+        className="flex-center md:bg-dark md:rounded-b-[40px] md:shadow-[0_20px_20px_-15px_rgba(255, 255, 255, 0.3)] p-2 md:p-4 border-2 border-grey rounded-[20px]"
       >
         <img src={urlFor(image && image[0])} alt="" className="w-28 md:w-3/4" />
       </Link>
@@ -33,9 +33,9 @@ const Product = ({
           ></path>
         </svg>
       </div> */}
-      <div className="flex flex-col p-4 min-h-[100px] md:flex-row md:items-end md:justify-around md:w-full">
+      <div className="flex flex-col p-4 min-h-[100px] md:flex-row md:items-end md:justify-evenly md:w-3/4">
         <div className="">
-          <p className="font-thin text-base">{name}</p>
+          <p className="font-thin text-base md:text-lg">{name}</p>
           <span className="font-bold text-lg">${price.toFixed(2)}</span>
         </div>
 
@@ -45,8 +45,12 @@ const Product = ({
         </div>
       </div>
 
-      <div className="absolute w-10 h-10 bg-blue right-0 bottom-0 rounded-ee-[20px] flex-center">
-        <AiOutlinePlus />
+      <div className="absolute w-10 h-10 bg-blue 
+      right-0 bottom-0 rounded-ee-[20px] 
+      flex-center hover:w-full hover:h-20 duration-300 cursor-pointer hover:rounded-es-[20px] group">
+        <AiOutlinePlus className="group-hover:text-3xl"/>
+
+        {/* <p className="hidden group-hover:block opacity-0 group-hover:opacity-100 delay-600">Add to Cart</p> */}
       </div>
     </div>
   );
