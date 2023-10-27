a310d552f4e6006dc9da6cb55a0c9db78651fce2
Pawel <pawel.bornikowski@gmail.com>
2023-10-27 17:26:50 +0200

    remove fire icon

--- components/products/Product.jsx
+++ components/products/Product.jsx
@@ -11,9 +11,9 @@ const Product = ({
   // max-w-[320px] h-[200px] lg:w-[350px] md:h-[450px] rounded-[30px] flex flex-col justify-between relative  bg-grey z-0 hover:scale-110 duration-200
   return (
     <div className="relative flex items-center  md:flex-col  w-[320px] md:w-[350px] md:h-[370px] rounded-[20px] ">
-      {hot ? (
+      {/* {hot ? (
         <BsFire className="absolute right-0 top-0 text-3xl text-red-600 md:right-4 md:top-4" />
-      ) : null}
+      ) : null} */}
       <Link
         href={`/product/${category.toLowerCase()}/${slug.current}`}
         className="flex-center  md:rounded-b-[40px]  p-2 md:p-4  rounded-[20px] "
