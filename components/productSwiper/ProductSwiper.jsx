b2ea99bc97cbd4008a7e7b2f26b3d969b092277a
Pawel <pawel.bornikowski@gmail.com>
2023-10-28 20:48:49 +0200

    add styling to template

--- components/productSwiper/ProductSwiper.jsx
+++ components/productSwiper/ProductSwiper.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
-
+import { urlFor } from "@/sanity/lib/client";
+import React from "react";
+import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
 const ProductSwiper = ({ products, category }) => {
+  const filterProducts = products.filter(
+    (product) => category === product.category && product
+  );
+
   return (
     <div>
-        <h3 className="w-full flex-center">{category}</h3>
-        <div className="w-full flex justify-around items-center h-[350px] bg-grey">
-            <div className="bg-green-600 h-[200px] w-[200px]"></div>
-            <div className="bg-red-600 h-full w-[350px]"></div>
-            <div className="bg-orange-500 h-[200px] w-[200px]"></div>
+      <h3 className="w-full flex-center">{category}</h3>
+      <div className="w-full flex justify-around items-center h-[400px] ">
+        <div className="h-[200px] w-[200px] relative z-0">
+          {" "}
+          <img src={urlFor(filterProducts[0].image[0])} />
+          <span className="absolute w-full h-[50px] bg-grey bottom-0 rounded-[50%] -z-10 left-[50%] translate-x-[-50%]"></span>
         </div>
-        <div className="flex w-full justify-around">
-            <div className="w-[200px] flex-center">prev</div>
-            <div className="w-[350px] flex-center flex-col">
-                <p>Jabra Elite 45h</p>
-                <p>$ 299</p>
-            </div>
-            <div className="w-[200px] flex-center">next</div>
+        <div className=" h-[350px] w-[350px] p-4 relative z-0">
+          <img src={urlFor(filterProducts[1].image[0])} className="z-2" />
+          <span className="absolute w-full h-[100px] bg-grey bottom-2 rounded-[50%] -z-10 left-[50%] translate-x-[-50%]"></span>
         </div>
+        <div className=" h-[200px] w-[200px] relative z-0">
+          {" "}
+          <img src={urlFor(filterProducts[2].image[0])} />
+          <span className="absolute w-full h-[50px] bg-grey bottom-0 rounded-[50%] -z-10 left-[50%] translate-x-[-50%]"></span>
+        </div>
+      </div>
+      <div className="flex w-full justify-around">
+        <div className="w-[200px] flex-center">
+          <span className="bg-grey w-14 h-14 flex-center rounded-full">
+            <BiSolidLeftArrow  className="text-3xl "/>
+          </span>
+        </div>
+        <div className="w-[350px] flex-center flex-col">
+          <p>Jabra Elite 45h</p>
+          <p>$ 299</p>
+        </div>
+        <div className="w-[200px] flex-center">
+          <span className="bg-grey w-14 h-14 flex-center rounded-full">
+            <BiSolidRightArrow className="text-3xl" />
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductSwiper
+export default ProductSwiper;
