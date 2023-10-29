0db6a4b712fa0e5dedbb0610b7870cba49b7106a
Pawel <pawel.bornikowski@gmail.com>
2023-10-29 15:35:43 +0100

    add both swiping

--- components/productSwiper/ProductSwiper.jsx
+++ components/productSwiper/ProductSwiper.jsx
@@ -9,14 +9,22 @@ const ProductSwiper = ({ products, category }) => {
   const [smallLeftIndex, setSmallLeftIndex] = useState(0);
   const [bigIndex, setBigIndex] = useState(1);
   const [smallRightIndex, setSmallRightIndex] = useState(2);
+
   const [lastIndex, setLastIndex] = useState(filterProducts.length);
-    console.log(lastIndex)
+
   const swipeNext = () => {
-      setSmallLeftIndex((prev) => (prev+1 >=  lastIndex ? 0 : prev + 1));
-      setBigIndex((prev) => (prev+1 >=  lastIndex ? 0 : prev + 1));
-      setSmallRightIndex((prev) => (prev+1 >=  lastIndex ? 0 : prev + 1));
-};
-return (
+    setSmallLeftIndex((prev) => (prev + 1 >= lastIndex ? 0 : prev + 1));
+    setBigIndex((prev) => (prev + 1 >= lastIndex ? 0 : prev + 1));
+    setSmallRightIndex((prev) => (prev + 1 >= lastIndex ? 0 : prev + 1));
+  };
+  const swipePrev = () => {
+    setSmallLeftIndex((prev) => (prev === 0 ? lastIndex - 1 : prev - 1));
+    setBigIndex((prev) => (prev === 0 ? lastIndex - 1 : prev - 1));
+    setSmallRightIndex((prev) => (prev === 0 ? lastIndex - 1 : prev - 1));
+  };
+
+  
+  return (
     <div>
       <h3 className="w-full flex-center">{category}</h3>
       <div className="w-full flex justify-around items-center h-[400px] ">
@@ -27,14 +35,14 @@ return (
         </div>
         <div className=" h-[350px] w-[350px] p-4 relative z-0">
           <img
-            src={urlFor(filterProducts[bigIndex].image[0])}
+            src={urlFor(filterProducts[bigIndex]?.image[0])}
             className="z-2"
           />
           <span className="absolute w-full h-[100px] bg-grey bottom-2 rounded-[50%] -z-10 left-[50%] translate-x-[-50%]"></span>
         </div>
         <div className=" h-[200px] w-[200px] relative z-0">
           {" "}
-          <img src={urlFor(filterProducts[smallRightIndex].image[0])} />
+          <img src={urlFor(filterProducts[smallRightIndex]?.image[0])} />
           <span className="absolute w-full h-[50px] bg-grey bottom-0 rounded-[50%] -z-10 left-[50%] translate-x-[-50%]"></span>
         </div>
       </div>
@@ -43,13 +51,14 @@ return (
           <button
             type="button"
             className="bg-grey w-14 h-14 flex-center rounded-full"
+            onClick={swipePrev}
           >
             <BiSolidLeftArrow className="text-3xl " />
           </button>
         </div>
         <div className="w-[350px] flex-center flex-col">
-          <p>Jabra Elite 45h</p>
-          <p>$ 299</p>
+          <p>{filterProducts[bigIndex].name}</p>
+          <p>$ {filterProducts[bigIndex].price}</p>
         </div>
         <div className="w-[200px] flex-center">
           <button
