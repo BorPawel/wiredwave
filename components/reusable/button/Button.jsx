24f5c3b26b622bb69891d52a1421c8be1fa4adf2
Pawel <pawel.bornikowski@gmail.com>
2023-11-10 17:04:57 +0100

    add button hover

--- components/reusable/button/Button.jsx
+++ components/reusable/button/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ buttonText, onClick, type }) => {
   const btnStroke = type === 'full' ? 'bg-blue text-white' : 'border-[1px] border-blue text-blue' 
   return (
     <button
-      className={`${btnStroke } rounded-[20px] font-bold px-20 py-5`}
+      className={`${btnStroke } rounded-[20px] font-bold px-20 py-5 hover:scale-110  duration-300`}
       onClick={onClick}
     >
       {buttonText}
