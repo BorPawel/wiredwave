04e9ce25c9ba19f8caf5e0f477ae7a25b751951e
Pawel <pawel.bornikowski@gmail.com>
2023-12-14 16:07:15 +0100

    chore: create categories page

--- components/categoriesHomepage/header/CategoryHeader.jsx
+++ components/categoriesHomepage/header/CategoryHeader.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Link from 'next/link'
 const CategoryHeader = () => {
   return (
     <div className="w-full flex justify-between items-center">
-        <h4>Shop by Categories</h4>
+      <h4>Shop by Categories</h4>
+      <Link href='/categories'>
         <p>Show all</p>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryHeader
+export default CategoryHeader;
