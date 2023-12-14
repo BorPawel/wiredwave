e79061d55c869124dc8a734b6d63bef6f75d07dc
Pawel <pawel.bornikowski@gmail.com>
2023-12-14 16:49:56 +0100

    feat: add category sorting feature

--- pages/categories/index.jsx
+++ pages/categories/index.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Checkbox from "@/components/categoriesPage/checkbox/Checkbox";
+import { client } from "@/sanity/lib/client";
+import React, { useState } from "react";
 
-const Categories = () => {
+const Categories = ({ products }) => {
+  const categoryArr = [
+    "Wireless",
+    "Gaming",
+    "Earphones",
+    "Speakers",
+    "Watches",
+  ];
+
+  const [selectedCheckbox, setSelectedCheckbox] = useState('Wireless');
+  const [checkedValue, setCheckedValue] = useState("Wireless");
+  const handleCheckboxChange = (selectedItem) => {
+    setSelectedCheckbox(selectedItem);
+    setCheckedValue(selectedItem);
+  };
+
   return (
-    <div>categories</div>
-  )
-}
+    <div className="min-h-[1000px] flex flex-col mt-12 p-2">
+      <div>
+        <div>Main / Categories</div>
+        <div className="w-full bg-black py-4 flex justify-between ite">
+          <div>Categories</div>
+          <div>Sort</div>
+        </div>
+      </div>
+      <div className="flex">
+        <div>
+          {categoryArr.map((item) => (
+            <Checkbox
+              key={item}
+              text={item}
+              isChecked={selectedCheckbox === item}
+              onChange={() => handleCheckboxChange(item)}
+            />
+          ))}
+        </div>
+        <div>
+          {products.map((item) =>
+            item.category === checkedValue ? <div>{item.name}</div> : null
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Categories
+export const getServerSideProps = async () => {
+  const productQuery = '*[_type == "product"]';
+  const products = await client.fetch(productQuery);
+
+  const saleQuery = '*[_type == "sale"]';
+  const sale = await client.fetch(saleQuery);
+
+  return {
+    props: {
+      products,
+
+      sale,
+    },
+  };
+};
+
+export default Categories;
