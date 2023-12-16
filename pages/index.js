30deccba4810c3e2dc1724a6a47c276f37833bec
Pawel <pawel.bornikowski@gmail.com>
2023-12-16 03:40:03 +0100

    refactor: clean up code in home component

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,24 @@
 import Banner from "@/components/banner/Banner";
 import HeadingText from "@/components/heading/HeadingText";
-import React, { useEffect, useState, useRef } from "react";
-import { client, urlFor } from "../sanity/lib/client";
-import Categories from "@/components/categories/Categories";
-
-import Product from "@/components/products/Product";
-
+import React from "react";
+import { client,  } from "../sanity/lib/client";
 import { useStateContext } from "@/context/StateContext";
-import ShoppingCart from "@/components/shoppingCart/ShoppingCart";
-import { LogosMarquee } from "@/components/marquee/LogosMarquee";
 import SaleBanner from "@/components/sale_banner/SaleBanner";
 import HotSales from "@/components/hotSales/HotSales";
-import { Navigation } from "@/components/product_details/navigation/Navigation";
-import ProductList from "@/components/products/productsList/ProductList";
-import ProductSwiper from "@/components/productSwiper/ProductSwiper";
 import CategoriesHomepage from "@/components/categoriesHomepage/CategoriesHomepage";
 
-const Home = ({ banner, products, logos, sale }) => {
-  const { showCart, category } = useStateContext();
+const Home = ({ banner, products, sale }) => {
 
-  const categoryArr = [
-    "Wireless",
-    "Gaming",
-    "Earphones",
-    "Speakers",
-    "Watches",
-  ];
+
   return (
     <>
-      {/* <HeadingText /> */}
-      <Banner banner={banner[0]}  />
+      <Banner banner={banner[0]} />
 
-  
-      <CategoriesHomepage  products={products}/>
-
-
-      {/* <div className="flex md:hidden flex-col gap-20">
-        {categoryArr.map((item, index) => (
-          <>
-            <ProductList key={index} products={products} category={item} />
-          </>
-        ))}
-      </div>
-
-      <div className="hidden md:flex flex-col gap-36">
-        {categoryArr.map((item, index) => (
-          <>
-            <ProductSwiper key={index} products={products} category={item} />
-           
-          </>
-        ))}
-      </div> */}
-      {/* <LogosMarquee logos={logos} /> */}
+      <CategoriesHomepage products={products} />
 
       <SaleBanner sale={sale} />
 
-      <HotSales products={products} text="Check out our hot selling products"/>
+      <HotSales products={products} text="Check out our hot selling products" />
     </>
   );
 };
@@ -66,9 +29,6 @@ export const getServerSideProps = async () => {
 
   const productQuery = '*[_type == "product"]';
   const products = await client.fetch(productQuery);
-
-  const logosQuery = '*[_type == "logos"]';
-  const logos = await client.fetch(logosQuery);
 
   const saleQuery = '*[_type == "sale"]';
   const sale = await client.fetch(saleQuery);
