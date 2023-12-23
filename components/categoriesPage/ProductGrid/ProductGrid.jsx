3903c607e8f53ef4a8aa33c0fbf6f2e7a443ea02
Pawel <pawel.bornikowski@gmail.com>
2023-12-23 14:47:55 +0100

    chore(ProductGrid): add link to navigate clicked product to product details page

--- components/categoriesPage/ProductGrid/ProductGrid.jsx
+++ components/categoriesPage/ProductGrid/ProductGrid.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ProductCard from '../ProductCard/ProductCard';
-
-
+import React from "react";
+import ProductCard from "../ProductCard/ProductCard";
+import Link from 'next/link'
 const ProductGrid = ({ products, checkedValue }) => {
   return products ? (
     <div className="w-full grid sm:grid-cols-2 md:grid-cols-3 gap-4">
       {products?.product.map((item) =>
-        item.category === checkedValue ? <ProductCard item={item} /> : null
+        item.category === checkedValue ? (
+          <Link href={`/product/${item.category.toLowerCase()}/${item.slug.current}`}>
+            <ProductCard item={item} />
+          </Link>
+        ) : null
       )}
     </div>
-  ) : null 
+  ) : null;
 };
 
 export default ProductGrid;
