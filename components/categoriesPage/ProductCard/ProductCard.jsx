09bd58ce6bce18708eda233f69596685d2542e4f
Pawel <pawel.bornikowski@gmail.com>
2023-12-28 16:31:34 +0100

    fix: add product card fields validation

--- components/categoriesPage/ProductCard/ProductCard.jsx
+++ components/categoriesPage/ProductCard/ProductCard.jsx
@@ -8,16 +8,16 @@ const ProductCard = ({ item }) => {
     <div className="aspect-square">
       <div className="w-full h-full p-4 shadow rounded flex justify-center items-center gap-2 flex-col">
         <img
-          src={urlFor(item.image && item.image[0])}
-          alt={item.name}
+          src={urlFor(item?.image && item.image[0])}
+          alt={item?.name}
           className="w-3/4"
         />
         <div className="w-3/4">
-          <p>{item.name}</p>
+          <p>{item?.name}</p>
           <div className="flex justify-between w-full">
-            <p>$ {item.price.toFixed(2)}</p>
+            <p>$ {item?.price?.toFixed(2)}</p>
             <div className="flex">
-              <p>{item.stars} </p>
+              <p>{item?.stars} </p>
               <AiFillStar className="text-2xl md:text-3xl text-blue" />
             </div>
           </div>
--- sanity/schemas/lib/category.js
+++ sanity/schemas/lib/category.js
@@ -6,8 +6,8 @@ export const category = {
         list: [
             {title: 'Wireless', value: 'wireless'},
             {title: 'Gaming', value: 'gaming'},
-            { title: 'Clip-On Headphones', value: 'clipOn'},
-            { title: 'Open-Ear Headphones', value: 'openEar'},
+            { title: 'Clip-On Headphones', value: 'clip-on'},
+            { title: 'Open-Ear Headphones', value: 'open-ear'},
             { title: 'Earbuds Headphones', value: 'earbuds'},
         ]
     }
