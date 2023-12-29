77b4a6b25e9c1428b5da52a3e6f40931b59cbfac
Pawel <pawel.bornikowski@gmail.com>
2023-12-29 16:19:03 +0100

    styles(Loading): Improve styles
    improve container of loading to fix jumping screen phase

--- app/category/loading.jsx
+++ app/category/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>loading</div>
+    <div className="m-auto w-full max-w-[1600px] flex justify-center items-center text-white min-h-[1100px]">Loading...</div>
   )
 }
 
