2477f0fdd0ffe3cad8a348dfd8fd01d6c9a52cdc
Pawel <pawel.bornikowski@gmail.com>
2023-12-29 16:43:40 +0100

    styles(navbar): change mobile styles

--- components/Modal/ProductCardModal/ProductCardModal.jsx
+++ components/Modal/ProductCardModal/ProductCardModal.jsx
@@ -1,7 +1,7 @@
 import ModalProductGallery from "./ModalProductGallery/ModalProductGallery";
 
 import ProductDetailsModal from "./ProductDetailsModal/ProductDetailsModal";
-import { IoMdClose } from "react-icons/io";
+
 const ProductCardModal = ({ product }) => {
   return (
     <div className="flex w-full flex-col md:flex-row relative ">
