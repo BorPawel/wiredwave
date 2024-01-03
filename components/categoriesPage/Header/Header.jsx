361535fcc912c5125eb7568dc1b74d75763268ca
Pawel <pawel.bornikowski@gmail.com>
2024-01-03 17:01:53 +0100

    refactor(categoryHeader): replace static text with backbutton

--- components/categoriesPage/Header/Header.jsx
+++ components/categoriesPage/Header/Header.jsx
@@ -1,12 +1,13 @@
 'use client'
 import React from 'react';
 import SortButton from './SortButton/SortButton';
+import BackButton from '@/components/product_details/BackButton/BackButton';
 
 const Header = () => {
   return (
     <div>
-      <div>Main / Category</div>
-      <div className="w-full py-10 flex justify-between items-center">
+      <BackButton />
+      <div className="w-full py-14 flex justify-between items-center">
         <h4>Category</h4>
        <SortButton />
       </div>
