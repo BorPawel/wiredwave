6d065af65ee0cae111ee859b3ad18bfa611237b3
Pawel <pawel.bornikowski@gmail.com>
2024-01-03 18:27:12 +0100

    chore: disable intersection Header on mobile

--- components/IntersectionHeader/IntersectionHeader.jsx
+++ components/IntersectionHeader/IntersectionHeader.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import BgHeader from "@/assets/images/bg-header.png";
 const IntersectionHeader = () => {
   return (
-    <div className="m-auto w-full max-w-[1600px] min-h-[1000px] text-white  my-32 image-container overflow-clip relative grid place-items-center z-0">
+    <div className="hidden m-auto w-full max-w-[1600px] min-h-[1000px] text-white  my-32 image-container overflow-clip relative md:grid place-items-center z-0">
       <div className="absolute w-full h-full  z-10 inset-blur"></div>
       <Image src={BgHeader} className="opacity-40 blur-effect absolute -z-1 " />
       <div className="w-3/4 h-full z-20 flex flex-col justify-center items-center">
