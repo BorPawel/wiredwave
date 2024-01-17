d0d00bcc936ad72ad947ec095bba44e5cc46d310
Pawel <pawel.bornikowski@gmail.com>
2024-01-17 15:25:03 +0100

    chore(searchOverlay): handle input typing

--- components/navbar/SearchOverlay/SearchOverlay.jsx
+++ components/navbar/SearchOverlay/SearchOverlay.jsx
@@ -1,9 +1,15 @@
 "use client";
 import { useStateContext } from "@/context/StateContext";
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { IoIosArrowBack } from "react-icons/io";
 const SearchOverlay = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
   const { showSearchOverlayHandler } = useStateContext();
   return ReactDOM.createPortal(
     <div className="w-screen min-h-screen bg-dark fixed right-0 top-0 z-50 flex-center overflow-auto text-white">
@@ -11,17 +17,22 @@ const SearchOverlay = () => {
         <div className="flex-center h-[250px] mt-6">
           <h2>Search</h2>
         </div>
-        <IoIosArrowBack
-          onClick={showSearchOverlayHandler}
-          className="text-3xl cursor-pointer"
-        />
-        <div className="w-full flex-center  ">
-          <input className="w-1/2 bg-dark border-[1px] border-white  rounded-md p-2" />
+
+        <div className="w-full flex-center relative">
+          <div className="absolute left-0">
+            <IoIosArrowBack
+              onClick={showSearchOverlayHandler}
+              className="text-3xl cursor-pointer"
+            />
+          </div>
+          <input
+            className="w-1/2 bg-dark border-[1px] border-white rounded-md p-2"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
         </div>
 
-        <div className="w-full flex-center ">
-            
-        </div>
+        <div className="w-[1400px] flex-center min-h-[500px] bg-black "></div>
       </div>
     </div>,
     document.getElementById("search-portal")
