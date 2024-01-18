9e8ad2af27f4d81155c638594d86781fdcdc1a5a
Pawel <pawel.bornikowski@gmail.com>
2024-01-18 16:08:39 +0100

    style: change categories header on homepage

--- components/categoriesHomepage/header/CategoryHeader.jsx
+++ components/categoriesHomepage/header/CategoryHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from 'next/link'
 const CategoryHeader = () => {
   return (
-    <div className="w-full flex justify-between items-center px-4 md:px-14">
+    <div className="w-full flex justify-between items-center px-4 ">
       <p className="text-xl md:text-2xl lg:text-3xl">Categories</p>
     </div>
   );
