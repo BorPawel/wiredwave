497071a7c47f5c0e91408b49114f5f0a58ea85c4
Pawel <pawel.bornikowski@gmail.com>
2024-02-29 20:06:55 +0100

    fix: properly set state of picked color

--- components/product_details/ProductDetails/AddToCard/AddToCard.jsx
+++ components/product_details/ProductDetails/AddToCard/AddToCard.jsx
@@ -2,21 +2,31 @@
 import Quantity from "@/components/reusable/Quantity";
 import Button from "@/components/reusable/button/Button";
 import { useStateContext } from "@/context/StateContext";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Colors from "./Colors/Colors";
 
 const AddToCard = ({ product }) => {
   const { addItemToCart, qty, color } = useStateContext();
 
-  const [pickedColor, setPickedColor] = useState(product?.colorImageArray[0]?.color?.hex)
-  
+  const [pickedColor, setPickedColor] = useState("");
+
+  useEffect(() => {
+    if (product.colorImageArray) {
+      setPickedColor(product?.colorImageArray[0]?.color?.hex);
+    }
+  }, [pickedColor]);
+
   return (
     <div className="flex flex-col gap-8 w-full  ">
       <div className="flex justify-start items-center gap-2">
-        <p>Quantity:</p>  
+        <p>Quantity:</p>
         <Quantity />
       </div>
-      <Colors product={product} setPickedColor={setPickedColor} pickedColor={pickedColor}/>
+      <Colors
+        product={product}
+        setPickedColor={setPickedColor}
+        pickedColor={pickedColor}
+      />
       <p className="font-bold w-1/2 text-5xl">${product?.price}</p>
       <div className="flex justify-around lg:justify-start items-center w-full lg:items-start gap-2  lg:gap-10 flex-wrap ">
         <Button
