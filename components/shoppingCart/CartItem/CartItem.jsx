8726682fb9987bea446cfa0bc70390e915c6d995
Pawel <pawel.bornikowski@gmail.com>
2024-02-29 20:13:58 +0100

    refactor: improve shopping cart item

--- components/shoppingCart/CartItem/CartItem.jsx
+++ components/shoppingCart/CartItem/CartItem.jsx
@@ -1,17 +1,13 @@
 "use client";
-import { urlFor } from "@/sanity/lib/client";
+
 import React from "react";
 import CartItemDetails from "./CartItemDetails/CartItemDetails";
-import CartItemQuantity from "./CartItemQuantity/CartItemQuantity";
-import CartItemRemove from "./CartItemRemove/CartItemRemove";
+
 import NextImage from "@/components/reusable/NextImage/NextImage";
 
 const CartItem = ({ item }) => {
-  console.log(item)
   return (
     <div className="flex w-full bg-grey rounded-[20px] p-2 ">
-      {/* <img src={urlFor(item.image && item.image[0])} className="h-20 lg:h-32" /> */}
-
       <NextImage
         image={item?.image[0]}
         className=""
@@ -20,17 +16,7 @@ const CartItem = ({ item }) => {
         height={100}
       />
 
-      <div className="flex  md:justify-between justify-evenly w-full px-2 md:px-6 flex-col">
-        <CartItemDetails item={item} />
-        <div className="flex items-center justify-between">
-          <p className="block font-thin">
-            ${(item?.price * item?.quantity).toFixed(2)}
-          </p>
-
-          <CartItemQuantity item={item} />
-          <CartItemRemove item={item} />
-        </div>
-      </div>
+    <CartItemDetails item={item}/>
     </div>
   );
 };
--- components/shoppingCart/CartItem/CartItemDetails/CartItemDetails.jsx
+++ components/shoppingCart/CartItem/CartItemDetails/CartItemDetails.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import CartItemQuantity from "../CartItemQuantity/CartItemQuantity";
+import CartItemRemove from "../CartItemRemove/CartItemRemove";
 
 const CartItemDetails = ({ item }) => {
   return (
-    <div className="flex flex-col  gap-1">
+    <div className="flex  md:justify-between justify-evenly w-full px-2 md:px-6 flex-col">
       <p className="font-bold">{item?.name}</p>
+      <div className="flex items-center justify-between">
+        <p className="block font-thin">
+          $ {(item?.price * item?.quantity).toFixed(2)}
+        </p>
+
+        <CartItemQuantity item={item} />
+        <CartItemRemove item={item} />
+      </div>
       <span className="flex items-center gap-2">
         <p className="font-thin">Color: </p>
         <span
@@ -13,7 +23,6 @@ const CartItemDetails = ({ item }) => {
           }}
         ></span>
       </span>
-     
     </div>
   );
 };
