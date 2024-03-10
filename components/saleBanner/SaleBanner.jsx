d7948867226656252fd4e9efb36a9534917439c5
Pawel <pawel.bornikowski@gmail.com>
2024-03-10 14:31:54 +0100

    styles: change sales banner styles

--- components/saleBanner/SaleBanner.jsx
+++ components/saleBanner/SaleBanner.jsx
@@ -9,13 +9,13 @@ const SaleBanner = async ( ) => {
 
   const data = await getData('sale');
   if(!data.sale){
-    return null
+    return <div>No sale</div>
   }
   const saleData = data.sale[0];
   const lowerCategory = saleData.category.toLowerCase();
   return (
     <div className="min-w-[320px] w-full p-2 my-32">
-      <div className="min-h-[400px]  bg-grey rounded-[30px] relative overflow-hidden flex flex-col md:flex-row justify-between">
+      <div className="min-h-[400px]  border-grey border-y-2 relative overflow-hidden flex flex-col md:flex-row justify-between">
         <div className="bg-blue w-[200px] h-full absolute left-[50%] translate-x-[-50%] -skew-x-[40deg] -z-1"></div>
         <SaleInfo sale={saleData} />
         <SaleImage image={saleData.image} />
